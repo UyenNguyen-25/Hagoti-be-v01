0c5f4880183041a771c03e33c800a068a64f0555
UyenNguyen-25 <khoang8666@gmail.com>
2024-09-14 00:45:28 +0700

    connect db

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
+const database = require("./config/db");
+const router = require("./routes/index");
+
+database.connect();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.use("/", router);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,5 @@
 const express = require("express");
-const authRoutes = require("../routes/authRoutes");
 
 const router = express.Router();
-
-router.use("/api", authRoutes);
 
 module.exports = router;
